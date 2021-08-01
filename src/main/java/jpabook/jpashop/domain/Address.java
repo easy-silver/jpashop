a3a3334476990b652520b76287d4cb80d9d92ba2
Timo <developer.timo@gmail.com>
2021-08-01 11:06:12 +0900

    엔티티 클래스 개발2 - 값 타입에 대한 제한 설정 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,6 +9,19 @@ import javax.persistence.Embeddable;
 public class Address {
 
     private String city;
+
     private String street;
+
     private String zipcode;
+
+    /** JPA가 사용하기 위한 기본 생성자
+     * 사용자는 건드릴 일이 없도록 protected로 제한한다. */
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
