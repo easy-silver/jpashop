45f5366041a95f86f48b5cca765afb411a0c0786
Timo <developer.timo@gmail.com>
2021-08-01 10:24:47 +0900

    엔티티 클래스 개발1 - 주요 엔티티 생성

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@DiscriminatorColumn(name = "dtype")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Entity
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+}
+
