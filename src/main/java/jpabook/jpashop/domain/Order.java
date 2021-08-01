73166dd1cc7cd5b1d7ae8d4618f85ea1e0778cb0
Timo <developer.timo@gmail.com>
2021-08-01 12:12:57 +0900

    엔티티 설계시 주의점 - 연관관계 편의 메서드 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,4 +33,22 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    /**
+     * 연관관계 편의 메서드
+     * (관계를 컨트롤 하는 쪽이 가지고 있는 게 좋다)
+     */
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
